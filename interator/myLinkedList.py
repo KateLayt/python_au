e462f413be61ff84ab54375e011c03df62eaa3c3
KateLayt <KateLayt@users.noreply.github.com>
2020-11-28 13:24:08 +0300

    ITERATOR-1022 Added myLinkedList.py

---
+++ interator/myLinkedList.py
@@ -0,0 +1,25 @@
+class Node:
+
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def get(self, index: int) -> int:
+        pass
+
+    def addAtHead(self, val: int) -> None:
+        pass
+
+    def addAtTail(self, val: int) -> None:
+        pass
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        pass
+
+    def deleteAtIndex(self, index: int) -> None:
+        pass
+
+    def printList(self):
+        pass
