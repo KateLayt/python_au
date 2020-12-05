55c4a987a829d891b61be077c958e8c336a23314
KateLayt <KateLayt@users.noreply.github.com>
2020-12-05 22:39:51 +0300

    HEXNUMBER-1022 Added calc.py and test_calc.py

---
+++ 16sum/calc.py
@@ -0,0 +1,109 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class HexNumber:
+    def __init__(self, string):
+        self.head = None
+        self.lenght = len(string)
+
+        if not (isUpper(string)):
+            print("Буквы в строке должны быть в верхнем регистре.")
+            return
+
+        for element in string:
+            if isnumber(element):
+                self.head = Node(element, self.head)
+            else:
+                return
+
+    def __str__(self):
+        string = ''
+        subhead = self.head
+        while subhead:
+            string += subhead.val
+            subhead = subhead.next
+        return string[::-1]
+
+    def add(self, secNumber):
+        if not (secNumber is HexNumber):
+            print("Сложение невозможно. Второе слагаемое не является числом.")
+            return
+
+        if self.lenght > secNumber.lenght:
+            len = self.lenght
+        else:
+            len = secNumber.lenght
+        if_rem = False
+
+        for i in range(len):
+            if self.head != None and secNumber.head != None:
+                num = makenumber(self.head.val) + makenumber(secNumber.head.val)
+                self.head = self.head.next
+                secNumber.head = secNumber.head.next
+
+            else:
+                if self.head != None and secNumber.head == None:
+                    num = makenumber(self.head.val)
+                    self.head = self.head.next
+
+                if self.head == None and secNumber.head != None:
+                    num = makenumber(secNumber.head.val)
+                    secNumber.head = secNumber.head.next
+
+            if if_rem:
+                num += 1
+                if_rem = False
+
+            if num > 15:
+                num = num - 16
+                if_rem = True
+
+            if i == 0:
+                resNumber = HexNumber(makechar(num))
+                real_head = resNumber.head
+            else:
+                resNumber.head.next = Node(makechar(num))
+                resNumber.head = resNumber.head.next
+
+        if if_rem:
+            resNumber.head.next = Node(makechar(1))
+        resNumber.head = real_head
+        return resNumber
+
+
+def isnumber(char):
+    if char.isdigit():
+        number = ord(char) - 48
+    else:
+        number = ord(char) - 55
+    if number > 15 or number < 0:
+        print("Не все введённые символы соответствуют обозначениям шестнадцатеричной системы счисления (012345ABCDEF)")
+        return False
+    else:
+        return True
+
+
+def makenumber(char):
+    if char.isdigit():
+        number = ord(char) - 48
+    else:
+        number = ord(char) - 55
+    return number
+
+
+def makechar(number):
+    if number < 10:
+        char = str(number)
+    else:
+        char = chr(number + 55)
+    return char
+
+
+def isUpper(string):
+    if string == string.upper():
+        return True
+    else:
+        return False
---
+++ 16sum/test_calc.py
@@ -0,0 +1,53 @@
+import unittest
+from calc import HexNumber
+
+
+class testing(unittest.TestCase):
+
+    def test_is_correct_string_low(self):
+        self.assertEqual("", str(HexNumber("abc")))
+
+    def test_is_correct_string_symbols(self):
+        self.assertEqual("", str(HexNumber("JKL")))
+
+    def test_is_correct_string(self):
+        self.assertEqual("ABC", str(HexNumber("ABC")))
+
+    def test_add_common(self):
+        num1 = HexNumber("123")
+        num2 = HexNumber("210")
+        expect = "333"
+        result = str(num1.add(num2))
+        self.assertEqual(expect, result)
+
+    def test_add_complicated(self):
+        num1 = HexNumber("A56")
+        num2 = HexNumber("711")
+        expect = "1167"
+        result = str(num1.add(num2))
+        self.assertEqual(expect, result)
+
+    def test_add_different_len(self):
+        num1 = HexNumber("12")
+        num2 = HexNumber("11111")
+        expect = "11123"
+        result = str(num1.add(num2))
+        self.assertEqual(expect, result)
+
+    def test_add_nothing(self):
+        num1 = HexNumber("1111")
+        num2 = HexNumber("")
+        expect = "1111"
+        result = str(num1.add(num2))
+        self.assertEqual(expect, result)
+
+    def test_add_none(self):
+        num1 = HexNumber("1111")
+        num2 = None
+        expect = None
+        result = num1.add(num2)
+        self.assertEqual(expect, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
